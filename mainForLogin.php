e891582a974dc99c37b2babb172dfc49ee50d64e
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-09-25 10:42:43 +0200

    Update mainForLogin.php

--- mainForLogin.php
+++ mainForLogin.php
@@ -1,12 +1,10 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <!-- importer le fichier de style -->
         <link rel="stylesheet" href="../public/css/styleForm.css" media="screen" type="text/css" />
     </head>
     <body style='background:#fff;'>
         <div id="content">
-            <!-- tester si l'utilisateur est connecté -->
             <?php
                 session_start();
                 if($_SESSION['username'] !== ""){
@@ -26,7 +24,6 @@
                 }
                 else if($_SESSION['username'] !== ""){
                     $user = $_SESSION['username'];
-                    // afficher un message
                     echo "<br>Bonjour $user, vous êtes connectés";
                 }
             ?>

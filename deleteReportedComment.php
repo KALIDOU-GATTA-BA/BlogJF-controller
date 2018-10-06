e1310a2e73f57c2f03a3497b7a9f8c5a6ecdb974
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-06 15:08:33 +0200

    Update deleteReportedComment.php

--- deleteReportedComment.php
+++ deleteReportedComment.php
@@ -1,9 +1,18 @@
 <?php 
-    require_once("model/Manager.php");
+	require_once("model/Manager.php");
     $dbAcess=new Manager;
     $bdd=$dbAcess->dbConnect();
     $id=$_GET['id'];
 
-    $req=$bdd->query("DELETE FROM comments where id=$id");
+    $req=$bdd->query("SELECT COUNT(comment) as nbCmt FROM comments WHERE post_id = $id ");
+    $data= $req->fetch();
+    $_= $data['nbCmt'];
+    
+	$req1=$bdd->query("DELETE FROM comments where id=$id");
+    
+    $req1 = $bdd->prepare("UPDATE posts SET countComment = :countCmt WHERE id = $id ");
+                  $req->execute(array(
+                  'countCmt' => $_
+                  ));
 
     echo 'Le commentaire a bien été supprimé. <br> <br> <a href="../view/backend/indexBackend.php">Retour</a>';

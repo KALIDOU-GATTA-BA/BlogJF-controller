ad6e5ac1509984f426666279980da011e81d3cd2
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-06 14:06:54 +0200

    Update frontend.php

--- frontend.php
+++ frontend.php
@@ -1,9 +1,8 @@
 <?php
 
-// Chargement des classes
 require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
-
+require_once("model/Manager.php");
 function listPosts()
 {
     $postManager = new PostManager(); 
@@ -25,6 +24,7 @@ function post()
 
 function addComment($postId, $author, $comment)
 {
+
     $commentManager = new CommentManager();
 
     $affectedLines = $commentManager->postComment($postId, $author, $comment);
@@ -32,7 +32,17 @@ function addComment($postId, $author, $comment)
     if ($affectedLines === false) {
         throw new Exception('Impossible d\'ajouter le commentaire !');
     }
-    else {
-        header('Location: index.php?action=post&id=' . $postId);
+    else {      
+             $dbAcess=new Manager;
+             $db=$dbAcess->dbConnect();
+             $req=$db->query("SELECT COUNT(comment) as nbCmt FROM comments WHERE post_id = $postId ");
+             $data= $req->fetch();
+             $insert= $data['nbCmt'];
+             $req1=$db->prepare("UPDATE posts SET countComment = :countCmt WHERE id = $postId ");
+             $req1->execute(array(
+             'countCmt' => $insert
+             )); 
+             header('Location: index.php?action=post&id=' . $postId);
     }
+    
 }
